d07c4a357792b6ee35bcf6278090b22fb8551f05
CoreOS Admin <core@ip-10-250-5-51.us-west-2.compute.internal>
2016-04-25 15:36:07 +0000

    .

--- test-docker.sh
+++ test-docker.sh
@@ -1,5 +1,5 @@
-docker stop riak-cs 2>/dev/null
-docker rm riak-cs 2>/dev/null
+#docker stop riak-cs 2>/dev/null
+#docker rm riak-cs 2>/dev/null
 
 /bin/true && docker run -d \
   --net=bridge \
@@ -9,5 +9,6 @@ docker rm riak-cs 2>/dev/null
   -p 41001:7946/udp \
   -p 8085:8085 \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  rickalm/riak-cs \
-  && docker exec -it riak-cs /bin/bash
+  rickalm/riak-cs
+
+docker exec -it riak-cs /bin/bash
